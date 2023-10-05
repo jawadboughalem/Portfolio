fec733105363fdbf815c5b6a3af86ad43b027c00
jawadboughalem <jawadboughalem91@gmail.com>
2023-10-05 13:41:59 +0200

    A jour

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Header from './components/Header';
+import About from './components/About';
+import Education from './components/Education';
+import Project from './components/Project';
+import Cv from './components/Cv';
+import Footer from './components/Footer';
+
+const root = document.getElementById('root');
+createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
